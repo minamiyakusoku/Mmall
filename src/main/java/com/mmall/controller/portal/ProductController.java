3c8f5830fc43d8d681498cb5260b378a1a43ac7b
akku <akku@akku.moe>
2017-05-18 10:13:39 +0800

    Initial commit

---
+++ src/main/java/com/mmall/controller/portal/ProductController.java
@@ -0,0 +1,52 @@
+package com.mmall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.service.IProductService;
+import com.mmall.vo.ProductDetailVo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by geely
+ */
+@CrossOrigin
+@Controller
+@Api(value = "/product", description = "商品相关操作")
+@RequestMapping("/product/")
+public class ProductController {
+
+    @Autowired
+    private IProductService iProductService;
+
+
+    @ApiOperation(value = "获取商品详细信息", notes = "根据商品id来获取商品详细信息")
+    @ApiImplicitParam(name = "productId", value = "商品id", required = true, dataType = "int")
+    @RequestMapping(value = "detail.do",method = RequestMethod.GET)
+    @ResponseBody
+
+    @CrossOrigin
+    public ServerResponse<ProductDetailVo> detail(Integer productId){
+        return iProductService.getProductDetail(productId);
+    }
+
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> list(@RequestParam(value = "keyword",required = false)String keyword,
+                                         @RequestParam(value = "categoryId",required = false)Integer categoryId,
+                                         @RequestParam(value = "pageNum",defaultValue = "1") int pageNum,
+                                         @RequestParam(value = "pageSize",defaultValue = "10") int pageSize,
+                                         @RequestParam(value = "orderBy",defaultValue = "") String orderBy){
+        return iProductService.getProductByKeywordCategory(keyword,categoryId,pageNum,pageSize,orderBy);
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/mmall/test/MainTest.java
@@ -0,0 +1,11 @@
+package com.mmall.test;
+
+
+/**
+ * Created by akku on 2017/5/17.
+ */
+public class MainTest
+{
+    public static void main(String[] args){
+    }
+}
